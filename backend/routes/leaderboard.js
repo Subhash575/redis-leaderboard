71d880b6825998c6644df3835209057f54dc590e
Subhash575 <subhash100393@gmail.com>
2026-06-16 16:35:33 +0530

    feat: add GET /leaderboard/:userId/rank endpoint

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -3,19 +3,20 @@ import client from "../config/redis.js";
 
 const router = Router();
 
+// endpoint for setting up and updating the score.
 router.post("/score", async (req, res) => {
   const { userId, score } = req.body;
 
   // validation
   if (!userId || score === undefined) {
-    res.status(400).json({ error: "userId and score are required" });
+    return res.status(400).json({ error: "userId and score are required" });
   }
 
   //converted score into number
   const points = Number(score);
 
   if (isNaN(points)) {
-    res.status(400).json({ error: "score must be a number" });
+    return res.status(400).json({ error: "score must be a number" });
   }
 
   try {
@@ -32,6 +33,7 @@ router.post("/score", async (req, res) => {
   }
 });
 
+// endpoint for getting the leaderboard value.
 router.get("/", async (req, res) => {
   try {
     // zrange return the flat array (if we use "REV").
@@ -59,6 +61,34 @@ router.get("/", async (req, res) => {
   } catch (err) {
     console.error("ZRANGE error:", err);
     res.status(500).json({ error: "Failed to fetch the leaderboard" });
+  }
+});
+
+// get the rank of the user by it userId
+router.get("/:userId/rank", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    //It will return the rank with highest score.
+    const rank = await client.zrevrank("leaderboard", userId);
+
+    // Here we use the `null` bcs according to redis behavior either it return value or 'null'
+    if (rank == null) {
+      return res
+        .status(404)
+        .json({ error: `User ${userId} not found in the leaderboard` });
+    }
+
+    // zcore for finding the score of user
+    const score = await client.zscore("leaderboard", userId);
+    res.json({
+      userId,
+      rank: rank + 1, // 0-based indexing
+      score: parseFloat(score),
+    });
+  } catch (err) {
+    console.error("ZREVRANK error: ", err);
+    res.status(500).json({ error: "failed to fetch rank" });
   }
 });
 
